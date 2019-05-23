c4e1569aa2788baa899b47d9e1ee77aa1cb635c0
Sebastian Pulak <pulakosso@gmail.com>
2019-05-23 20:53:36 +0200

    flatlist + searchbar with swapi/people

--- src/Main.js
+++ src/Main.js
@@ -1,79 +1,114 @@
 import React, {Component} from 'react'
-import { StyleSheet, Text, View, ActivityIndicator, AppRegistry, ScrollView, TouchableOpacity, Dimensions} from 'react-native'
-import firebase from 'react-native-firebase'
-import {Navigation} from 'react-native-navigation';
+import { StyleSheet, Text, View,  ActivityIndicator, AppRegistry, FlatList, Dimensions, StatusBar} from 'react-native'
 var width = Dimensions.get('window').width; //full width
 var height = Dimensions.get('window').height; //full height
+import { ListItem, SearchBar } from 'react-native-elements';
+
 
 type Props = {};
 export default class Main extends Component<Props> {
     constructor() {
         super();
         this.state = {
-            isLoading: true
-          }
+          loading: false,
+          data: [],
+          error: null,
+          isLoading: true
+        };
+        this.arrayholder = [];
       }
-
 
       componentDidMount(){
-        return fetch('https://swapi.co/api/people/')
-          .then((response) => response.json())
-          .then((responseJson) => {
-            this.setState({
-              isLoading: false,
-              dataSource: responseJson
-            }, function() {
-                console.log(this.state.dataSource.results.length);
-            });
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-
+        this.callApi();
       }
- 
-  render() {
-    if(this.state.isLoading){
+
+      callApi = () => {
+        const url = 'https://swapi.co/api/people/?format=json';
+        this.setState({ loading: true });
+    
+        fetch(url)
+          .then(res => res.json())
+          .then(res => {
+            this.setState({
+              data: res.results,
+              error: res.error || null,
+              loading: false,
+            });
+            this.arrayholder = res.results;
+          })
+          .catch(error => {
+            this.setState({ error, loading: false });
+          });
+      };
+
+      searchFilterFunction = text => {
+        this.setState({
+          value: text,
+        });
+    
+        const newData = this.arrayholder.filter(item => {
+          const itemData = `${item.name.toUpperCase()}`;
+          const textData = text.toUpperCase();
+    
+          return itemData.indexOf(textData) > -1;
+        });
+        this.setState({
+          data: newData,
+        });
+      };
+    
+      renderSeparator = () => {
         return (
-            <View style={styles.container}>
-              <Text>Loading</Text>
-              <ActivityIndicator size="large" />
+          <View
+            style={{
+              height: 1,
+              width: '86%',
+              backgroundColor: '#CED0CE',
+              marginLeft: '14%',
+            }}
+          />
+        );
+      };
+      renderHeader = () => {    
+        return (      
+          <SearchBar        
+            placeholder="Type Here..."        
+            lightTheme        
+            round        
+            onChangeText={text => this.searchFilterFunction(text)}
+            autoCorrect={false} 
+            value={this.state.value}           
+          />    
+        );  
+      };
+      render() {
+        if (this.state.loading) {
+          return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+              <ActivityIndicator />
             </View>
-          )
+          );
         }
-
-        let row = [];
-        let rowsOfTiles = [];
-
-        for (let i = 0; i < this.state.dataSource.results.length; i++){
-            row.push(
-                <View key={i}>
-                  <TouchableOpacity style={styles.tile}>
-                    <Text style={styles.tileTextName}>{this.state.dataSource.results[i].name}</Text>
-                  </TouchableOpacity> 
-                </View>
-              );
-              rowsOfTiles.push(
-                <View style={styles.rowOfTiles} key={i}>
-                  {row}
-                </View>
-              ); 
-              row = [];
-              if (i === this.state.dataSource.results.length - 1) {
-                rowsOfTiles.push(
-                  <View style={styles.rowOfTiles} key={i}>
-                    {row}
-                  </View>  
-                )
-              } 
-              return (
-              <ScrollView contentContainerStyle={styles.container}>
-                  {rowsOfTiles}
-              </ScrollView>
-              )
-        }
+        return (
+          
+          <View style={{ flex: 1 }}>
+            <StatusBar hidden />
+            <FlatList
+              data={this.state.data}
+              renderItem={({ item }) => (
+                <ListItem
+                  title={`${item.name}`}
+                />
+              )}
+              keyExtractor={item => item.name}
+              ItemSeparatorComponent={this.renderSeparator}
+              ListHeaderComponent={this.renderHeader}
+            />
+          </View>
+        );
       }
     }
+  
 
     
 
